dbd2940bd3a1215ebb01cb8c4833ab32a89a9be6
Vihan Chaudhry <vihanchaudhry@gmail.com>
2015-02-04 18:01:17 -0800

    Added a ground. Added GameState enum.

--- src/Levels.cpp
+++ src/Levels.cpp
@@ -3,9 +3,13 @@
 Levels::Levels()
 {
 	// Load background texture and assign to rectangle shape
-	bgTexture.loadFromFile("../assets/background.png"); 
+	bgTexture.loadFromFile("../assets/sprites/background.png"); 
+	groundTexture.loadFromFile("../assets/sprites/ground.png");
 	bgRect.setSize(sf::Vector2f(1280,720));	
-	bgRect.setTexture(&bgTexture);
+	bgRect.setTexture(&bgTexture); 
+	groundRect.setSize(sf::Vector2f(1280, 16));
+	groundRect.setTexture(&groundTexture);
+	groundRect.setPosition(0, 635);
 }
 
 void Levels::update()
@@ -16,4 +20,5 @@ void Levels::update()
 void Levels::render(sf::RenderWindow &window)
 {
 	window.draw(bgRect);
+	window.draw(groundRect);
 }
