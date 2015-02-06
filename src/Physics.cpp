5d10007d39fea45f161e0feb96214a1c41150be8
planetguy <planetguy32@users.noreply.github.com>
2015-02-06 12:39:16 -0800

    Primitive, sloppy but hopefully functional collision detection. (A library to do this would be helpful.)

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -3,20 +3,61 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
-std::vector< sf::FloatRect > activeWorldHitboxes;
+std::vector< sf::FloatRect * > activeWorldHitboxes;
 
+inline float sq(float f)
+{
+   return f*f;
+}
+
+inline float min(float f1,  float f2)
+{
+   return f1>f2 ? f2  : f1;
+}
+
+//Start with dumb algorithm for now, will replace if causes lag
 void doPhysics(sf::FloatRect& hitbox, float& velocityX, float& velocityY)
 {
-   
-   for(int i=0; i<activeWorldHitboxes.size(); i++)
+   static const int STEPS=20;
+
+   float stepX=velocityX/STEPS;
+   float stepY=velocityY/STEPS;
+
+   for(int i=0; i<STEPS; i++)
    {
-      
+      for(int i=0; i< activeWorldHitboxes.size(); i++)
+      {
+         sf::FloatRect * checking=activeWorldHitboxes[i];
+         if( checking -> intersects(hitbox))
+         {
+            float leastDiffX=min(
+                  sq(checking -> top - hitbox.top),
+                  sq(checking -> top + checking -> height - (hitbox.top + hitbox.height))
+            );
+            float leastDiffY=min(
+                  sq(checking -> left - hitbox.left),
+                  sq(checking -> left + checking -> width - (hitbox.left + hitbox.width))
+            );
+            if(leastDiffX>leastDiffY)
+            {
+               stepX=0;
+            }
+            else
+            {
+               stepY=0;
+            }
+            if(stepX==0 && stepY==0)
+              return; //stuck
+         }
+      }
+      hitbox.left+=stepX;
+      hitbox.top+=stepY;
    }
 }
 
 void registerEntity(sf::FloatRect& hitbox)
 {
-   activeWorldHitboxes.insert(activeWorldHitboxes.end(), hitbox);
+   activeWorldHitboxes.insert(activeWorldHitboxes.end(), & hitbox);
 }
 
 void clearHitboxes()
