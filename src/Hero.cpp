8348bfd6984c09dc79f7493837923c1acc401012
fonicartist <fonicartist@aim.com>
2015-02-09 17:24:50 -0800

    Walking animation added.

--- src/Hero.cpp
+++ src/Hero.cpp
@@ -9,7 +9,7 @@ Hero::Hero()
 	heroSprite.setOrigin(32, 128);
 	heroSprite.setPosition(1280, 1360);
 	sf::Vector2f velocity(sf::Vector2f(0, 0));
-	sf::Vector2i direct(sf::Vector2i(faceRight, 1));
+	sf::Vector2i anim(sf::Vector2i(0, 1));
 
 	// Initialize basic hero stats
 	heroHP = HERO_BASE_HP;
@@ -18,23 +18,56 @@ Hero::Hero()
 	heroSpeed = HERO_BASE_SPEED * heroSpeedMultiplier;
 	jumpTimer = 0; //Timer for jump function duration
 	jumpCooldown = 0;
+	xFrame = 0;
+	yFrame = 0;
+	frameTimer = 0;
+	faceRight = true;
+}
+
+void Hero::walkAnim()
+{
+	//Determine row in sprite sheet
+	if (faceRight)
+		anim.y = 1;
+	else
+		anim.y = 2;
+	//Move frame forward
+	if (frameTimer > 4)
+	{
+		if (xFrame != 4 && xFrame != 9)
+		{
+			frameTimer = 0;
+			xFrame++;
+		}
+		else if (frameTimer > 6)
+		{
+			frameTimer = 0;
+			xFrame++;
+		}
+		if (xFrame == 10)
+			xFrame = 0;
+	}
+	anim.x = xFrame;
+	frameTimer++;
 }
 
 void Hero::left()
 {
 	velocity.x = -heroSpeed;
 	heroSprite.move(velocity.x, 0.f);
+	walkAnim();
 }
 
 void Hero::right()
 {
 	velocity.x = heroSpeed;
 	heroSprite.move(velocity.x, 0.f);
+	walkAnim();
 }
 
 void Hero::jump(float seconds)
 {
-	if (jumpTimer < 20 && jumpCooldown > 2)
+	if (jumpTimer < 22 && jumpCooldown > 2)
 	{
 		velocity.y = -350;
 		velocity.y += GRAVITY*seconds*jumpTimer*2;
@@ -57,16 +90,31 @@ void Hero::update(float seconds)
 		// Handle movement
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
-			direct.x = faceLeft;
+			faceRight = false;
+			anim.x = 1;
 			left();
 		}
 		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
-			direct.x = faceRight;
+			faceRight = true;
+			anim.x = 0;
 			right();
 		}
 		else
+		{
 			velocity.x = 0;
+			xFrame = 0;
+			if (faceRight)
+			{
+				anim.x = 0; 
+				anim.y = 0;
+			}
+			else
+			{
+				anim.x = 1;
+				anim.y = 0;
+			}
+		}
 
 		// Jump
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
@@ -96,6 +144,6 @@ void Hero::update(float seconds)
 
 void Hero::render(sf::RenderWindow &window)
 {
-	heroSprite.setTextureRect(sf::IntRect(direct.x * 64, direct.y * 128, 64, 128));
+	heroSprite.setTextureRect(sf::IntRect(anim.x * 64, anim.y * 128, 64, 128));
 	window.draw(heroSprite);
 }
--- src/Hero.h
+++ src/Hero.h
@@ -9,19 +9,22 @@ private:
 	sf::Texture heroTexture;
 	sf::Sprite heroSprite;
 	sf::Vector2f velocity;
-	sf::Vector2i direct;
-
-	enum Direction { faceRight, faceLeft };
+	sf::Vector2i anim;		//Tracker for hero sprite frames
 
 	int heroHP;
-	int jumpTimer;
-	int jumpCooldown;
+	int xFrame;					//Current frame in the x grid
+	int yFrame;					//Current frame in the y grid
+	int frameTimer;				//Counter for sprite frame duration
+	int jumpCooldown;			//Cooldown counter for jump
+	int jumpTimer;				//Counter for jump duration
+	bool faceRight;				//Boolean for determining direction hero is facing at rest
 	bool isAlive;
 	bool inAir;
 	float heroSpeed;
 	float heroSpeedMultiplier;
 	float heroJumpSpeed = 80;
 
+	void walkAnim();			//Animation sequence for walking
 	void left();
 	void right();
 	void jump(float seconds);
