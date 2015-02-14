d482e9bce9a270b39f7b77cd7e85ec82c6b2ee2e
HiDaN4 <hidan446@gmail.com>
2015-02-14 02:21:44 -0800

    Implement BackgroundGenerator

--- src/Levels.cpp
+++ src/Levels.cpp
@@ -3,13 +3,17 @@
 Levels::Levels()
 {
 	// Load background texture and assign to rectangle shape
-	bgTexture.loadFromFile("../assets/sprites/background.png"); 
+	BackgroundGenerator::instance().LoadTextureForLevelOrDie(1); // current level 1; just testing
+
+	Background.texture = BackgroundGenerator::instance().background_texture(); // assign the texture
+	Background.texture.setRepeated(true);
+	Background.rectangle.setSize(sf::Vector2f(2560, 1440));
+	Background.rectangle.setTexture(&Background.texture);
+	Background.rectangle.setTextureRect({ 0, 0, 5500, 3000 });
+
+
 	groundTexture.loadFromFile("../assets/sprites/GroundTexture.png");
-	bgRect.setSize(sf::Vector2f(2560, 1440));
-	bgRect.setTexture(&bgTexture);
-	bgRect.setTextureRect({ 0, 0, 5500, 3000 });
 	groundTexture.setRepeated(true);
-	bgTexture.setRepeated(true);
 	groundRect.setSize(sf::Vector2f(2560, 64));
 	groundRect.setTexture(&groundTexture);
 	groundRect.setTextureRect({ 0, 0, 1000, 32 });
@@ -27,8 +31,35 @@ void Levels::update()
 
 void Levels::render(sf::RenderWindow &window)
 {
-	window.draw(bgRect);
+	window.draw(Background.rectangle);
+	//window.draw(bgRect);
 	window.draw(groundRect);
 
 	platform.render(window);
+}
+
+
+std::string BackgroundGenerator::GetFileName(LevelNum level_num)
+{
+	std::string filename;
+	switch (level_num)
+	{
+	case 1: return filename = "../assets/sprites/background.png";
+	case 2: return filename = "../assets/sprites/background2.png";
+	default: return filename; // it is "" when initialized
+		break;
+	}
+}
+
+void BackgroundGenerator::LoadTextureForLevelOrDie(LevelNum level)
+{
+	std::string filetoload = this->GetFileName(level);
+
+	if (filetoload != "") {
+		background_texture_.loadFromFile(filetoload);
+	}
+	else {
+		return; // TODO: notify about dying
+		// throw "Cannot load the texture!";
+	}
 }
--- src/Levels.h
+++ src/Levels.h
@@ -4,6 +4,10 @@
 #include <SFML/Graphics.hpp>
 #include "Platform.h"
 
+class BackgroundGenerator;
+
+using LevelNum = int;
+
 class Levels
 {
 private:
@@ -11,6 +15,11 @@ private:
 	sf::Texture groundTexture;
 	sf::RectangleShape bgRect;
 	sf::RectangleShape groundRect;
+	struct Background
+	{
+		sf::Texture texture;
+		sf::RectangleShape rectangle;
+	} Background;
 
 public:
 	Platform platform;
@@ -23,6 +32,36 @@ public:
 
 	const sf::FloatRect getGroundRect()
 	{ return groundRect.getGlobalBounds(); }
+};
+
+/*************************************************
+*	BackgroundGenerator
+*	
+*	 Loads a background sprite for given level
+*
+*************************************************/
+class BackgroundGenerator
+{
+private:
+	BackgroundGenerator() {}
+
+	sf::Texture background_texture_;
+
+	std::string GetFileName(LevelNum level_num);
+
+public:
+	static BackgroundGenerator& instance()
+	{
+		static BackgroundGenerator *instance_ = new BackgroundGenerator(); // initializes only once
+		return *instance_;
+	}
+
+	~BackgroundGenerator() {}
+
+	void LoadTextureForLevelOrDie(LevelNum level);
+
+	const sf::Texture& background_texture() const { return background_texture_; }
+
 };
 
 #endif
